615becade0bf11639bfd8056d9e55ee765149599
AntonLettuce <antonlettuce@gmail.com>
2022-11-29 16:09:17 +0400

    add tag building method

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,6 +3,12 @@
 require_relative "hexlet_code/version"
 
 module HexletCode
-  class Error < StandardError; end
-  # Your code goes here...
+  module Tag
+    def self.build tag, params = []
+      attributes = params.map { |attribute, value| "#{attribute}=\"#{value}\"" }.join(' ')
+      divider = attributes.empty? ? '' : ' '
+      content_w_closing_tag = "#{yield}</#{tag}>" if block_given?
+      "<#{tag}#{divider}#{attributes}>#{content_w_closing_tag}"
+    end
+  end
 end
